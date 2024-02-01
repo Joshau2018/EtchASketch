f7ffdf884f2fd735259215aa4ba35c36c147b589
Joshau2018 <113706963+Joshau2018@users.noreply.github.com>
2024-02-01 14:44:26 -0600

    Work in progress
    we just started

---
+++ main.py
@@ -0,0 +1,14 @@
+# Python
+from turtle import Turtle, Screen, mainloop
+
+class Etch:
+
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+    
+if __name__ == '__main__':
+    draw = Etch()
+    draw.run()
